87bc6a3a459a97faa43c6dab1ebca3f5d974232e
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-11-23 16:55:46 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+  var theVault = {
+  };
+
+  function setValue(key, value){
+      theVault[key] = value;
+      return theVault;
+    }
+
+  function getValue(key){
+      if(theVault.hasOwnProperty(key)){
+        return theVault[key];
+      } else {
+        return null;
+      }
+    }
+
+  return{
+    setValue: setValue,
+    getValue: getValue
+  };
+
+
 };
